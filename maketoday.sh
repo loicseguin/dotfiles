04dba5812fee443ca90216beff1d8d9b0d95ef65
Loïc Séguin-C <lsc@loicseguin.com>
2018-01-27 13:33:18 -0500

    Cleanup

--- maketoday.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Create a notebook directory for today. Add a symlink in $HOME to make it
-# easier to access.
-
-cd $HOME
-today=`date "+%Y/%m%d"`
-mkdir -p $HOME/notebook/$today
-rm $HOME/today
-ln -s $HOME/notebook/$today today
